17d37e442f2f15fe1b3478ec28cd6f57b1d14df6
jacobmott <jacobmott@gmail.com>
2023-08-21 01:34:46 -0500

    Saving progress on section:
    Section 10: Different Weapon Types
    finished submachine gun and shotgun

--- Blaster/Source/Blaster/Weapon/WeaponTypes.h
+++ Blaster/Source/Blaster/Weapon/WeaponTypes.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#define TRACE_LENGTH 80000.f
+
 UENUM(BlueprintType)
 enum class EWeaponType : uint8
 {
 	EWT_AssaultRifle UMETA(DisplayName = "Assault Rifle"),
 	EWT_RocketLauncher UMETA(DisplayName = "Rocket Launcher"),
 	EWT_Pistol UMETA(DisplayName = "Pistol"),
+	EWT_SubmachineGun UMETA(DisplayName = "Submachine Gun"),
+	EWT_Shotgun UMETA(DisplayName = "Shotgun"),
 
 	EWT_MAX UMETA(DisplayName = "DefaultMAX")
 };
